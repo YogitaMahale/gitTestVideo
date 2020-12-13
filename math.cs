0a9ab8264cb53c9f30628c7551b49f1e94fe2523
Yogtiamahalee <mahaleyogita233@gmail.com>
2020-12-13 14:33:42 +0530

    commit math class

---
+++ math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gitTestVideoEX
+{
+    public class math
+    {
+        public void add(int a,int b)
+        {
+            int res = a + b;
+
+        }
+    }
+}
